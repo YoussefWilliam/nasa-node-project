965312be3cdeefec13652058ac18af9cd81b1f97
YoussefWilliam <youssef.william997@gmail.com>
2023-10-27 16:37:35 +0200

    :tada: create launches + their pages

---
+++ server/src/models/launches.model.js
@@ -0,0 +1,39 @@
+const launches = new Map();
+
+const launch1 = {
+  flightNumber: 100,
+  mission: "Kepler Exploration X",
+  rocket: "Explorer IS1",
+  launchDate: new Date("December 27, 2030"),
+  destination: "Kepler-442 b",
+  customers: ["ZTM", "NASA"],
+  upcoming: true,
+  success: true,
+};
+const launch2 = {
+  flightNumber: 26,
+  mission: "A great mission with no purpose",
+  rocket: "Explorer ZIZ",
+  launchDate: new Date("December 27, 2012"),
+  destination: "Kepler-442 b",
+  customers: ["ZTM", "NASA"],
+  upcoming: true,
+  success: true,
+};
+
+const launch3 = {
+  flightNumber: 10,
+  mission: "Apollo Moon Exploration",
+  rocket: "Apollo 13",
+  launchDate: new Date("November 11, 1997"),
+  destination: "Kepler-442 b",
+  customers: ["ZTM", "NASA"],
+  upcoming: false,
+  success: true,
+};
+
+launches.set(launch1.flightNumber, launch1);
+launches.set(launch2.flightNumber, launch2);
+launches.set(launch3.flightNumber, launch3);
+
+module.exports = { launches };
--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,8 +1,8 @@
 const { planets } = require("../../models/planets.model");
 
-function getAllPlanets(req, res) {
+const getAllPlanets = (req, res) => {
   return res.status(200).json(planets);
-}
+};
 
 module.exports = {
   getAllPlanets,
