bc2a4a5d01432ac43148a83a7b8b1a9c4c1dcc19
YoussefWilliam <youssef.william997@gmail.com>
2023-10-27 13:40:12 +0200

    :fire: render fetched planets in frontend

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,15 @@
 const http = require("http");
 const app = require("./app");
+const { loadPlanetsData } = require("./models/planets.model");
 
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 8000;
 
+async function startServer() {
+  await loadPlanetsData();
+}
+
 server.listen(PORT, () => console.log(`🚀 Listening to port ${PORT} 🚀`));
+
+startServer();
